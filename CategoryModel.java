98f254b25db627011ef3226cfeaebd906fe5e005
ShubhamThapa30 <79707421+ShubhamThapa30@users.noreply.github.com>
2021-07-12 17:59:43 +0530

    Add files via upload

---
+++ CategoryModel.java
@@ -0,0 +1,32 @@
+package navbar.model;
+
+public class CategoryModel {
+	private Integer id;
+	private String name;
+	private Integer parent_id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getParent_id() {
+		return parent_id;
+	}
+
+	public void setParent_id(Integer parent_id) {
+		this.parent_id = parent_id;
+	}
+
+}
