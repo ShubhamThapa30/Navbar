deeba92101b5c0b399c390793583d4ee1618d454
ShubhamThapa30 <79707421+ShubhamThapa30@users.noreply.github.com>
2021-07-12 17:58:06 +0530

    Add files via upload

---
+++ CodiNavbarApplication.java
@@ -0,0 +1,13 @@
+package com.navbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodiNavbarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodiNavbarApplication.class, args);
+	}
+
+}
